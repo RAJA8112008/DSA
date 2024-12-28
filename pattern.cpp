bbc4001f5803a8d29fde8c2fe7fe0b7216b83a8e
Raja <kraj9380286@gmail.com>
2024-12-28 11:02:28 +0530

    commited

--- pattern.cpp
+++ pattern.cpp
@@ -3,16 +3,18 @@ using namespace std;
 int main(){
     int n =4;
 for(int i=0;i<n;i++){
-    for(int j=0;j<n-1-i;j++){
-        cout<<" "<<" ";
+    for(int j=0;j<n-i-1;j++){
+        cout<<" ";
     }
     for(int j=1;j<=i+1;j++){
-        cout<<j<<" ";
+        cout<<"*"<<" ";
     }
-for(int j=i;j>0;j--){
-    cout<<j<<" ";
-}
-cout<<endl;
+    if(i!=0){
+    for(int j=i;j<=n;j++){
+        cout<<" ";
+    }
+    }
+    cout<<endl;
 }
 return 0;
 }
