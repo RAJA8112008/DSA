4384bc6b7e6b771469da28c97fbb2320cfe92232
Raja <kraj9380286@gmail.com>
2024-12-28 09:38:43 +0530

    commited

--- pattern.cpp
+++ pattern.cpp
@@ -1,14 +1,13 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
+int main(){
+    int n =5;
+for(int i=1;i<=n;i++){
+    for(int j=i;j<=i+1;j++){
+cout<<i+1<<" ";
 
-int main() {
-int n=4;
-for(int i=0;i<n;i++){
-for(int j=i+1;j<=n;j++){
-    cout<<j+1;
-    i++;
-}
-cout<<endl;
+    }
+   cout<<endl;
 }
 return 0;
 }
