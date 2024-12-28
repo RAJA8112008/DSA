93a314b76482b9a105bbb1c85c15a0de694bb0a7
Raja <kraj9380286@gmail.com>
2024-12-28 12:02:21 +0530

    commited

--- pattern.cpp
+++ pattern.cpp
@@ -11,5 +11,15 @@ cout<<"*";
     }
     cout<<endl;
 }
+
+for(int i=0;i<n-1;i++){
+for(int j=0;j<=i;j++){
+    cout<<" ";
+}
+for(int j=1;j<=2*(n-1-i)-1;j++){
+    cout<<"*";
+}
+cout<<endl;
+}
 return 0;
 }
