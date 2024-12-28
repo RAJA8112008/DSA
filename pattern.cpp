0d2fab47add65415907833e100f95008a3158f18
Raja <kraj9380286@gmail.com>
2024-12-28 16:36:44 +0530

    commited

--- pattern.cpp
+++ pattern.cpp
@@ -1,25 +1,25 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n =5;
-for(int i=0;i<n;i++){
-    for(int j=1;j<=n-1-i;j++){
-        cout<<" ";
+    int n=4;
+    for(int i=1;i<=n;i++){
+        for(int j=0;j<=n-i-1;j++){
+            cout<<" ";
+        }
+        for(int j=0;j<n;j++){
+            cout<<"*";
+        }
+        if(i!=1){
+            for(int j=n-1;j<=n-1;j++){
+                cout<<" ";
+            }
+            if(i!=1){
+                for(int j=0;j<=n-1;j++){
+                    cout<<"*"<<" ";
+                }
+            }
+        }
+        cout<<endl;
     }
-    for(int j=1;j<=2*i+1;j++){
-cout<<"*";
-    }
-    cout<<endl;
-}
-  //for bottom
-for(int i=0;i<n-1;i++){
-for(int j=0;j<=i;j++){
-    cout<<" ";
-}
-for(int j=1;j<=2*(n-1-i)-1;j++){
-    cout<<"*";
-}
-cout<<endl;
-}
-return 0;
+    return 0;
 }
