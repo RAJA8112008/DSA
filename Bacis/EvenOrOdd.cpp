f34c0f522553f4a8e377ed9c0a18c1ef8da673c2
Raja <kraj9380286@gmail.com>
2025-01-16 18:34:58 +0530

    commited

--- Bacis/EvenOrOdd.cpp
+++ Bacis/EvenOrOdd.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int n =252;
-    for(int i=n;i>=n;i--){
+    int n =251;
+    for(int i=n; i>=n;i--){
 cout<<i<<endl;
     }
     return 0;
