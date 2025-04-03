0d67be41b322a4c32868a1ebf4b1096ba58f24b5
Raja <kraj9380286@gmail.com>
2025-04-03 21:50:10 +0530

    commited

--- Bacis/Switch/switch.cpp
+++ Bacis/Switch/switch.cpp
@@ -4,8 +4,9 @@ int main(){
     int num=5;
     switch(num){
         case 2:cout<<"raja"<<endl;
+break;
         case 5:cout<<"kumar"<<endl; 
-        break;
+        
         case 3:cout<<"saniya"<<endl;
         case 4:cout<<"raja2"<<endl;
     }
