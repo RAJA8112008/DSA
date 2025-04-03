feb7a0d9ce5c2076e9f4417dc5557db93d00a66f
Raja <kraj9380286@gmail.com>
2025-04-03 21:46:45 +0530

    commited

--- Bacis/Switch/switch.cpp
+++ Bacis/Switch/switch.cpp
@@ -1,9 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int ch ='2';
-    switch(ch){
-case '2': cout<<"first"<<endl;
-case '3':cout<<"second"<<endl;
-    }
+    int num=5;
+    switch(num)
+    
 }
