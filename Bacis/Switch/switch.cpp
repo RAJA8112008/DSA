636c2c33cfdd42424399a16cb9e418a22aaa6bf3
Raja <kraj9380286@gmail.com>
2025-04-05 21:24:48 +0530

    commited

--- Bacis/Switch/switch.cpp
+++ Bacis/Switch/switch.cpp
@@ -1,12 +1,11 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int a,b;
-    cin >>a >>b;
+    int a,b; 
+    cin>>a>>b;
     int ans=1;
     for(int i=1;i<=b;i++){
-        ans = ans * a;
+        ans=ans*a;
     }
-    
-    cout<<ans<<endl;
+    cout<<"answer of a"<<ans <<endl;
 }
