0ee8ed98827026ed9a174bd42ff832f46cb7193e
Raja <kraj9380286@gmail.com>
2025-04-06 00:12:15 +0530

    commited

--- Bacis/Switch/switch.cpp
+++ Bacis/Switch/switch.cpp
@@ -1,23 +1,20 @@
 #include<iostream>
 using namespace std;
-  
 int factorial(int n){
     int factorial=1;
     for(int i=1;i<=n;i++){
         factorial=factorial*i;
     }
-        return factorial;
+    return factorial;
 }
-    int ncr(int n,int r){
-        int num= factorial(n);
-        int demunet=factorial(r)*factorial(n-r);
-        int ans=num/demunet;
-        return ans;
-    }
-
-
+int ncr(int n,int r){
+    int num=factorial(n);
+    int demuneter=factorial(r)*factorial(n-r);
+    int ncr=num/demuneter;
+    return ncr;
+}
 int main(){
-int n,r;
-cin>>n>>r;
- cout<<"Factorial of num:"<<ncr(n,r)<<endl;
+    int n,r;
+    cin>>n>>r;
+cout<<"value of ncr:"<<ncr(n,r)<<endl;
 }
