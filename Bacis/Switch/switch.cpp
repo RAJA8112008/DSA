8fe26f0847e6a7a8a7d3e9eda75fe741fcd668fa
Raja <kraj9380286@gmail.com>
2025-04-05 23:05:38 +0530

    commited

--- Bacis/Switch/switch.cpp
+++ Bacis/Switch/switch.cpp
@@ -1,19 +1,22 @@
 #include<iostream>
 using namespace std;
-
-
-
-int power(){
-    int a,b;
-    cin>>a>>b;
-    int ans=1;
-    for(int i=1;i<=b;i++){
-        ans= ans *a;
-    }
-    return ans;
+bool isEven(int a){
+  if(a&1){
+    return 0;
+  } 
+  else{
+    return 1;
+  }
 }
-int main(){
 
-cout<<"value of a" <<power()<<endl;
-return 0;
+
+int main(){
+    int num;
+    cin>>num;
+   bool isEven(num)
+    cout<<"num is even"<<a<<endl;
+   
+    cout<<"num is odd"<<a<<endl;
+   }
+    
 }
