560238b090d1eeb8b0693ef6ab7120b6a95e7041
Raja <kraj9380286@gmail.com>
2025-04-06 15:13:25 +0530

    commited

--- Bacis/Switch/switch.cpp
+++ Bacis/Switch/switch.cpp
@@ -1,12 +1,21 @@
 #include<iostream>
 using namespace std;
-void printcounting(int n){
-    for(int i=1;i<=n;i++){
-   cout<<i<<" "<<endl;
+bool isprime(int n){
+    for(int i=2;i<n;i++){
+        if(n&i==1){
+            return 1;
+        }
+        
     }
-}
+    return 0;
+} 
 int main(){
     int n;
-    cin>>n; 
-    printcounting(n);
+    cin>>n;
+    if(isprime(n)){
+        cout<<"Prime"<<endl;
+    }
+    else{
+        cout<<"not prime"<<endl;
+    }
 }
