82f1f86fab1afa504d1dd62e0880f97e3351a102
Raja <kraj9380286@gmail.com>
2025-04-07 23:20:43 +0530

    commited

--- Bacis/Switch/switch.cpp
+++ Bacis/Switch/switch.cpp
@@ -1,24 +1,31 @@
 #include<iostream>
 using namespace std;
 
-int totalsum(int sumnum[],int n){
-    int ans=0;
-    for(int i=0;i<n;i++){
-        
-         
-        ans= ans+sumnum[i];
+// This function calculates the sum of elements in the array
+int totalsum(int num[], int n) {
+    int isum = 0; // Variable to store the total sum
+    for(int i = 0; i < n; i++) {
+        isum += num[i]; // Adding each element to isum
     }
-    
-   return ans;
+    return isum; // Return the final sum
 }
 
+int main() {
+    int n; // To store how many elements we want in the array
+    cin >> n; // User inputs the size of the array
 
-int main(){
-    
-    int sumnum[5]={1,2,3,4,5};
-   int result= totalsum(sumnum,5);
+    int arr[n]; // Declaring an array of size n
 
-cout<<result<<endl;
-return 0;
+    // Taking n elements as input from the user
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i]; // Store each number in the array
+    }
 
+    // Call the totalsum function and store the result in 'result'
+    int result = totalsum(arr, n);
+
+    // Print the final result
+    cout << result << endl;
+
+    return 0;
 }
