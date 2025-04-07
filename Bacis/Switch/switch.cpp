0d2d2c619db5a267f4d371fbce6addbd6fdccf3c
Raja <kraj9380286@gmail.com>
2025-04-08 00:02:13 +0530

    commited

--- Bacis/Switch/switch.cpp
+++ Bacis/Switch/switch.cpp
@@ -1,6 +1,21 @@
 #include<iostream>
 using namespace std;
+  
+bool search( int arr[],int n){
+    if(n==arr[n]){
+        return 1;
+    }
+    return 0;
+}
+
 int main(){
-    int arr[5]={2,2,3,4,5}
-    
+    int n;
+    cin>>n;
+    int arr[5]={1,2,3,4,5}
+     
+   if(search( arr,n)){
+    cout<<"value exist"<<endl;
+   }
+cout<<"value not essist"<<endl;
+return 0;
 }
