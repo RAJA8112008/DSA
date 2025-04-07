844b7cac6b500f09f65c3e3f0505f907d82c4577
Raja <kraj9380286@gmail.com>
2025-04-07 23:48:18 +0530

    commited

--- Bacis/Switch/switch.cpp
+++ Bacis/Switch/switch.cpp
@@ -1,20 +1,6 @@
-# include<iostream>
+#include<iostream>
 using namespace std;
-int totalsum(int arr[],int n){
-    int sum=0;
-    for(int i=0;i<n;i++){
-        sum= sum + arr[i];
-    }
-    return sum;
-}
 int main(){
-    int n;
-    cin>>n;
-    int arr[n];
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-    }
-    int result= totalsum(arr,n);
-     cout<<result<<endl;
-     return 0;
+    int arr[5]={2,2,3,4,5}
+    
 }
