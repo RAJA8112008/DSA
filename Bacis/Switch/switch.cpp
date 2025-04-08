d72e6d424006c75ebe2a1c38eee7a787dcb5206b
Raja <kraj9380286@gmail.com>
2025-04-08 17:41:50 +0530

    commited

--- Bacis/Switch/switch.cpp
+++ Bacis/Switch/switch.cpp
@@ -1,28 +1,28 @@
 #include<iostream>
 using namespace std;
-  
-int swaparr(int arr[],int n){
-int start = 0;
-int end = n-1;
-while(start < end){
-    swap(arr[start],arr[end]);
-        start ++;
-        end--;
-    }
-}
 
+void swaparr(int arr[],int n){
+    int start=0;
+    int end=n-1;
+    while(start<=end){
+    swap(arr[start],arr[end]);
+    start++;
+    end--;
+}
+}
 
 void printarr(int arr[],int n){
-for( int i=0;i<n;i++){
-cout<<arr[i]<<" ";
-}
-cout<<endl;
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
 }
 
+
 int main(){
-    int arr[5]={2,5,6,8,7};
-    
-    swaparr(arr,5);
-    printarr(arr,5);
+    int arr[6]={4,5,6,2,8,3};
+    printarr( arr,6);
+    swaparr(arr,6);
+    printarr(arr,6);
     return 0;
 }
