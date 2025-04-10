035f56e47c6bc1a9b5901e1be2a9fee8d3f1a2a5
Raja <kraj9380286@gmail.com>
2025-04-10 23:21:34 +0530

    commited

--- Bacis/Switch/switch.cpp
+++ Bacis/Switch/switch.cpp
@@ -1,18 +1,28 @@
-#include <bits/stdc++.h> 
-vector<int> findArrayIntersection(vector<int> &arr1, int n, vector<int> &arr2, int m)
-{
-	vector<int>ans;
-	for(int i=0;i<n;i++){
-
-	int element=arr1[i];
-
-		for(int j=0;i<m;j++){
-			if(element==arr2[j]){
-				ans.push_back(element);
-				arr2[j]=-15249;
-				break;
-			}
+#include<iostream>
+using namespace std;
+int solve(int arr[],int size,int key){
+	int start =0;
+	int end= size-1; 
+	int mid =(start+end)/2;
+	while(start<=end){
+		
+		if(arr[mid]<key){
+			int start =mid+1;
 		}
+			else if(arr[mid]>key){
+				int end=mid-1;
+		}
+		else{
+			return mid;
+		}
+		int mid =(start+end)/2;
 	}
-	return ans;
+	return -1;
+}
+int main(){
+int evenarr[5]={2,4,6,8,10};
+ int result=solve(evenarr,5,10);
+ cout<<"Key value"<<result<<endl;
+ return 0;
+
 }
