7f204aa793818dc7d285e17b022084a83fb663df
Raja <kraj9380286@gmail.com>
2025-04-11 21:06:24 +0530

    commited

--- Bacis/Switch/switch.cpp
+++ Bacis/Switch/switch.cpp
@@ -1,28 +1,25 @@
 #include<iostream>
 using namespace std;
+int firstOcurrence(int arr[],int n,int key){
+	int start=0;
+	int end=n-1;
+	int mid =(start+end)/2;
+	while(start<=end){
+		if(arr[mid]<=key){
+			start=mid+1;
+		}
+		else if(arr[mid]>key){
+			end=mid-1;
+		}
+		else{
+			return mid;
+		}
+	}
+	return 
+}
 
-int solve(int arr[],int size,int key){
-int start =0;
-int end=size-1;
-int mid =(start+end)/2;
- 
-while(start<=end){
-	if(arr[mid]<key){
-		start =mid+1;
-	}
-	else if(arr[mid]>key){
-		end =mid-1;
-	}
-	else{
-		return mid;
-		 mid =(start+end)/2;
-	}
-}
-return -1;
-}
+
 int main(){
-	int oddarr[6]={2,4,6,8,10,12};
-	 int result=  solve(oddarr,6,10);
-	 cout<<"fining value"<<result<<endl;
-return 0;
+	int Binaryarr[6]={1,2,3,4,4,5};
+
 }
