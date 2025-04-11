ad792947e21fc41770f1047cf2e863ae16df3a07
Raja <kraj9380286@gmail.com>
2025-04-11 21:29:29 +0530

    commited

--- Bacis/Switch/switch.cpp
+++ Bacis/Switch/switch.cpp
@@ -3,23 +3,30 @@ using namespace std;
 int firstOcurrence(int arr[],int n,int key){
 	int start=0;
 	int end=n-1;
-	int mid =(start+end)/2;
+	int ans=-1;
+	int mid = start+(end-start)/2;
 	while(start<=end){
-		if(arr[mid]<=key){
-			start=mid+1;
+		if(arr[mid]==key){
+			ans = mid;
+			end = mid-1;
 		}
-		else if(arr[mid]>key){
+		else if(key>arr[mid]){
+			start = mid+1;
+		}
+		else if(key<arr[mid]){
 			end=mid-1;
 		}
-		else{
-			return mid;
-		}
+			mid = start+(end-start)/2;
+		
 	}
-	return 
+	return ans;
 }
 
 
 int main(){
 	int Binaryarr[6]={1,2,3,4,4,5};
+	int result =firstOcurrence(Binaryarr,6,4);
+	cout<<"index of 4 "<<result<<endl;
+	return 0;
 
 }
