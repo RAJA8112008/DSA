6b172abd6f85423dba84fcbc009571b569f03e95
Raja <kraj9380286@gmail.com>
2025-04-11 17:27:45 +0530

    commited

--- Bacis/Switch/switch.cpp
+++ Bacis/Switch/switch.cpp
@@ -1,29 +1,30 @@
 #include<iostream>
 using namespace std;
+
 int solve(int arr[],int size,int key){
-	int start =0;
-	int end= size-1; 
-	// int mid =(start+end)/2;
-	while(start<=end){
-		int mid =(start+end)/2;
-		if(arr[mid]<key){
-			int start =mid+1;
-		}
-			else if(arr[mid]>key){
-				int end=mid-1;
-		}
-		else{
-			return mid;
-			
-		}
-		
-	}
-	return -1;
-}
-int main(){
-int evenarr[5]={2,4,6,8,10};
- int result=solve(evenarr,5,3);
- cout<<"Key value"<<result<<endl;
+int start =0;
+int end=size-1;
+int mid =(start+end)/2;
  
- return 0;
+while(start<=end){
+	if(arr[mid]<key){
+		start =mid+1;
+	}
+	else if(arr[mid]>key){
+		end =mid-1;
+	}
+	else{
+		return mid;
+		 mid =(start+end)/2;
+	}
+}
+return -1;
+}
+
+
+int main(){
+	int oddarr[6]={2,4,6,8,10,12};
+	 int result=  solve(oddarr,6,10);
+	 cout<<"fining value"<<result<<endl;
+return 0;
 }
