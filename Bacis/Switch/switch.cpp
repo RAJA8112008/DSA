85abdf398a985a38b345fb64c8a220e7321cf12c
Raja <kraj9380286@gmail.com>
2025-04-13 00:13:04 +0530

    commited

--- Bacis/Switch/switch.cpp
+++ Bacis/Switch/switch.cpp
@@ -1,54 +1,21 @@
 #include<iostream>
 using namespace std;
-
-int firstOcurrence(int arr[], int n, int key) {
-	int start = 0, end = n - 1;
-	int ans = -1;
-
-	while (start <= end) {
-		
-		int mid = start + (end - start) / 2;
-		if (arr[mid] == key) {
-			ans = mid;
-			end = mid - 1;
+int getPivot(int arr[],int n){
+	int s=0;
+	int e=n-1;
+	while(s<e){
+		int mid =s+(e-s)/2;
+		if(arr[mid]>=arr[0]){
+			s= mid+1;
 		}
-		else if (key > arr[mid]) {
-			start = mid + 1;
-		}
-		else {
-			end = mid - 1;
+		else{
+			e=mid;
 		}
 	}
-	return ans;
+	return s;
 }
-
-int lastOcurrence(int arr[], int n, int key) {
-	int start = 0, end = n - 1;
-	int ans = -1;
-	while (start <= end) {
-		int mid = start + (end - start) / 2;
-		if (arr[mid] == key) {
-			ans = mid;
-			start = mid + 1;
-		}
-		else if (key > arr[mid]) {
-			start = mid + 1;
-		}
-		else {
-			end = mid - 1;
-		}
-	}
-	return ans;
-}
-
-int main() {
-	int Binaryarr[6] = {1, 2, 3, 4, 4, 5};
-
-	int first = firstOcurrence(Binaryarr, 6, 4);
-	int last = lastOcurrence(Binaryarr, 6, 4);
-
-	cout << "Index of first 4: " << first << endl;
-	cout << "Index of last 4: " << last << endl;
-
-	return 0;
+int main(){
+	int arr[7]={9,7,8,5,6,1,2};
+	int result=getPivot(arr,6);
+	cout<<"pivot value:"<<result<<endl;
 }
