a5e4be262e10ff36de004faafbc5f92ff3f21177
Raja <kraj9380286@gmail.com>
2025-04-14 22:16:35 +0530

    commited

--- Bacis/Switch/switch.cpp
+++ Bacis/Switch/switch.cpp
@@ -1,42 +1,8 @@
-int getPivot(vector<int>& arr,int n){
-    int s=0;
-    int e=n-1;
-    while(s<e){
-        int mid=s+(e-s)/2;
-        if(arr[mid]>=arr[0]){
-            s=mid+1;
-        }
-        else{
-            e=mid;
-        }
+#include<iostream>
+using namespace std;
+int main(){
+    int n=10;
+    for(int i=1;i<=n;i++){
+        cout<<i<<endl;
     }
-    return s;
-}
-int binarySearch(vector<int>& arr,int start,int end,int key){
-    int s =start;
-    int e =end;
-    while(s<=e){
-        int mid=s+ (e-s)/2;
-        if(arr[mid]>key){
-            e=mid-1;
-        }
-        else if(arr[mid]<key){
-            s=mid+1;
-        }
-        else{
-            return mid;
-        }
-    }
-    return -1;
-}
-
-int search(vector<int>& arr, int n, int k)
-{
-   int pivot=getPivot(arr,n);
-  if(k>=arr[pivot] && k<=arr[n-1]) {
-       return binarySearch(arr,pivot,n-1,k);
-   }
-   else{
-       return binarySearch(arr,0,pivot-1,k);
-   }
 }
