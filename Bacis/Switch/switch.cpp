fb1be2d5f6f0b6e6808b7d7c73e046d42e7e42d8
Raja <kraj9380286@gmail.com>
2025-04-19 07:51:12 +0530

    commited

--- Bacis/Switch/switch.cpp
+++ Bacis/Switch/switch.cpp
@@ -1,8 +1,26 @@
 #include<iostream>
 using namespace std;
-int main(){
-    int n=11;
-    for(int i=1;i<=n;i++){
-        cout<<i<<endl;
+
+void  getsort(int arr[],int n){
+    
+    for(int i=1;i<n-1;i++){
+        int mininum=i;
+        for(int j=i+1;j<n;j++){
+            if(arr[mininum]>arr[j]){
+                swap(arr[i],arr[j]);
+            }
+        }
     }
+}
+
+int main(){
+    int n=5;
+    
+    int arr[5]= {10,8,9,1,5};
+    getsort(arr,n);
+    for(int i=1;i<n;i++){
+        cout<<arr[i]<<endl;
+    }
+    return 0;
+    
 }
