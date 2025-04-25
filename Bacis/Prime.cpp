68e403e88145f3eeed54b5a786fd3f2b74d04607
Raja <kraj9380286@gmail.com>
2025-04-25 22:31:19 +0530

    commited

--- Bacis/Prime.cpp
+++ Bacis/Prime.cpp
@@ -1,9 +1,18 @@
 #include<iostream>
 using namespace std;
+
+int getlength(char name[]){
+    int count =0;
+    for(int i=0;name[i]!='/0';i++){
+        count++;
+    }
+    return count;
+}
+
 int main(){
    char name[10];
-   
    cin>>name;
-   name[2] ='/0';
    cout<<"my name"<<name<<endl;
+   cout<<"length"<<getlength(name)<<endl;
+   return 0;
 }
