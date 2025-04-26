f3ca2d941d8f689a71f866d7e7a247ce2c8c174d
Raja <kraj9380286@gmail.com>
2025-04-26 15:55:24 +0530

    commited

--- Bacis/Prime.cpp
+++ Bacis/Prime.cpp
@@ -1,19 +1,54 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int getlength(char name[]){
-    int count =0;
-    for(int i=0;name[i]!='/0';i++){
-        count++;
+// Correct: function to check if a character is alphanumeric
+bool convertor(char s) {
+    if ((s >= 'a' && s <= 'z') || (s >= '0' && s <= '9') || (s >= 'A' && s <= 'Z')) {
+        return true;
     }
-    return count;
+    return false;
 }
 
-int main(){
-    
-   char name[10];
-   cin>>name;
-   cout<<"my name"<<name<<endl;
-   cout<<"length"<<getlength(name)<<endl;
-   return 0;
+// Correct: function to manually convert uppercase to lowercase
+char towler(char s) {
+    if (s >= 'A' && s <= 'Z') {
+        s = s + 32; // 'A' + 32 = 'a', 'B' + 32 = 'b', etc
+    }
+    return s;
 }
+
+// Correct: main function to check palindrome
+bool checkPalindrome(string s) {
+    int st = 0;
+    int end = s.size() - 1;
+    
+    while (st < end) {
+        // Skip non-alphanumeric characters
+        if (!convertor(s[st])) {
+            st++;
+        }
+        else if (!convertor(s[end])) {
+            end--;
+        }
+        else {
+            // Compare after converting to lowercase
+            if (towler(s[st]) != towler(s[end])) {
+                return false;
+            }
+            st++;
+            end--;
+        }
+    }
+    return true;
+}
+
+int main() {
+    string s;
+    getline(cin, s);
+    if (checkPalindrome(s)) {
+        cout << "Palindrome" << endl;
+    } else {
+        cout << "Not a palindrome" << endl;
+    }
+}
+
