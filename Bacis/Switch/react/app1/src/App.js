55411161cf93d892c9978cc864f7a0fedd32b6b5
Raja <kraj9380286@gmail.com>
2025-04-28 23:17:12 +0530

    commited

--- Bacis/Switch/react/app1/src/App.js
+++ Bacis/Switch/react/app1/src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Item from './components/Item';
 
 function App() {
-  return (
-    <div className="App">
-      hello ji
-    </div>
+  return ( 
+    <div>
+      <Item></Item>
+      <div className="App">hello ji</div>
+    </div> 
   );
 }
-
 export default App;
