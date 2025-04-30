414532a38cff741b57eaed7d56e40b974cf3d032
Raja <kraj9380286@gmail.com>
2025-04-30 17:53:25 +0530

    commited

--- Bacis/Switch/react/app1/src/App.js
+++ Bacis/Switch/react/app1/src/App.js
@@ -3,14 +3,37 @@ import './App.css';
 import Item from './components/Item';
 import ItemDate from './components/ItemDate';
 function App() {
+  const responce=[
+    {
+      ItemName:"nirma1",
+      ItemDay:"28",
+      ItemMonth:"june",
+      ItemYear:"2008"
+
+    },
+    {
+      ItemName:"nirma2",
+      ItemDay:"281",
+      ItemMonth:"juney",
+      ItemYear:"2008"
+
+    },
+    {
+      ItemName:"nirma3",
+      ItemDay:"285",
+      ItemMonth:"junek",
+      ItemYear:"2008"
+
+    }
+  ];
   return ( 
     <div className='mfg-Date'>
-      <Item name="Nirma"></Item>
-      <ItemDate day="15" month="August" year="1947"></ItemDate>
-      <Item name="Surfexcell"></Item>
-      <ItemDate day="23" month="May" year="1987"></ItemDate>
-      <Item name="555"></Item>
-      <ItemDate day="16" month="September" year="1957"></ItemDate>
+      <Item name={responce[0].ItemName}></Item>
+      <ItemDate day={responce[0].ItemDay} month={responce[0].ItemMonth} year={responce[0].ItemYear}></ItemDate>
+      <Item name={responce[1].ItemName}></Item>
+      <ItemDate day={responce[1].ItemDay} month={responce[1].ItemMonth} year={responce[1].ItemYear}></ItemDate>
+      <Item name={responce[2].ItemName}></Item>
+      <ItemDate day={responce[2].ItemDay} month={responce[2].ItemMonth} year={responce[2].ItemYear}></ItemDate>
       <div className="App">hello ji</div>
     </div> 
   );
