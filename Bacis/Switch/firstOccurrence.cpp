a3dddeb099958959e7026d41457bf6bea973169c
Raja <kraj9380286@gmail.com>
2025-05-02 18:12:14 +0530

    commited

--- Bacis/Switch/firstOccurrence.cpp
+++ Bacis/Switch/firstOccurrence.cpp
@@ -1,32 +1,15 @@
-#include <iostream>
+#include<iostream>
+#include<algorithm>
+#include<cctype>
 using namespace std;
 
-int firstOccurrence(int arr[], int n, int key) {
-    int start = 0, end = n - 1;
-    int ans = -1;
-
-    while (start <= end) {
-        int mid = start + (end - start) / 2;
-
-        if (arr[mid] == key) {
-            ans = mid;
-            end = mid - 1;  // keep searching on the left
-        }
-        else if (key > arr[mid]) {
-            start = mid + 1;
-        }
-        else {
-            end = mid - 1;
-        }
-    }
-
-    return ans;
+bool caseInsensitiveCompare(char a, char b) {
+    return tolower(a) < tolower(b);
 }
 
 int main() {
-    int Binaryarr[6] = {1, 2, 3, 4, 4, 5};
-    int result = firstOccurrence(Binaryarr, 6, 4);
-    cout << "Index of 4: " << result << endl;
-
+    string s = "Keshav";
+    sort(s.begin(), s.end(), caseInsensitiveCompare);
+    cout << s << endl;
     return 0;
 }
