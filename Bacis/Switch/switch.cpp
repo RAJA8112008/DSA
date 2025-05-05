ed9a389d41e4d6ce02cb12ef33eb32d000fda777
Raja <kraj9380286@gmail.com>
2025-05-05 21:42:54 +0530

    commited

--- Bacis/Switch/switch.cpp
+++ Bacis/Switch/switch.cpp
@@ -1,19 +1,14 @@
 # include<stdio.h>
 int  main(){
  int a,b,c;
-
- 
  scanf("%d",&a);
  scanf("%d",&b);
  scanf("%d",&c);
-
- 
 if(a<b && a<c){
-    printf("small number=%",a);
+    printf("small number:%",a);
 }else if(b<c){
-   printf("print b=%d",b);
-}else{
-    
-    printf("print c=%d",c);
+   printf("print b:%d",b);
+}else{  
+    printf("print c:%d",c);
 }
 }
