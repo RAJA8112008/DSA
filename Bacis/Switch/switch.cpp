1fc722c83bf4541b1c89134b71ed02e7ffeb8ddc
Raja <kraj9380286@gmail.com>
2025-05-06 21:42:10 +0530

    commited

--- Bacis/Switch/switch.cpp
+++ Bacis/Switch/switch.cpp
@@ -1,31 +1,31 @@
 #include<stdio.h>
-int main()
-{
-   int day;
-   printf("Enter the day number:");
-   scanf("%d",&day);
-   if(day==1){
-    printf("\nSunday");
-   }
-   else if(day==2){
-    printf("\nMonday");
-   }
-   else if(day==3){
-    printf("\n Tuesday");
-   }
-   else if(day==4){
-    printf("\nwednesday");
-   }
-   else if(day==5){
-    printf("\n Tuesday");
-   }
-   else if(day==6){
-    printf("\nFriday");
-   }
-   else if(day==7){
-    printf("\n Saturday");
-   }
-   else{
-    printf("Day is not found:");
-   }
+int main(){
+    int day;
+    printf("value of day:");
+    scanf("%d",&day);
+    switch(day){
+        case 1:
+        printf("\nMonday");
+        break;
+        case 2:
+        printf("\nTuesday");
+        break;
+        case 3:
+        printf("\nWednesday");
+        break;
+        case 4:
+        printf("\nThursday");
+        break;
+        case 5:
+        printf("\nFriday");
+        break;
+        case 6:
+        printf("\nSaturday");
+        break;
+        case 7:
+        printf("\nSunday");
+        break;
+        default:
+        printf("\nvalue is not found");
+    }
 }
