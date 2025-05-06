90a5e96cb13c00c53fa38c844c3bc5272d491f1c
Raja <kraj9380286@gmail.com>
2025-05-06 21:27:14 +0530

    commited

--- Bacis/Switch/switch.cpp
+++ Bacis/Switch/switch.cpp
@@ -1,12 +1,31 @@
 #include<stdio.h>
 int main()
 {
-   int year;
-   printf("value of year:");
-   scanf("%d",&year);
-   if((year%4==0 && year%100!=0)||(year%400==0)){
-    printf("\nyear is leep:%d",year);
-   }else{
-    printf("\nyear is not leep:%d",year);
+   int day;
+   printf("Enter the day number:");
+   scanf("%d",&day);
+   if(day==1){
+    printf("\nSunday");
+   }
+   else if(day==2){
+    printf("\nMonday");
+   }
+   else if(day==3){
+    printf("\n Tuesday");
+   }
+   else if(day==4){
+    printf("\nwednesday");
+   }
+   else if(day==5){
+    printf("\n Tuesday");
+   }
+   else if(day==6){
+    printf("\nFriday");
+   }
+   else if(day==7){
+    printf("\n Saturday");
+   }
+   else{
+    printf("Day is not found:");
    }
 }
