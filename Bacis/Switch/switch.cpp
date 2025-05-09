da5e6d5b76fe4af894253c8ce8ac93bf532a3a7d
Raja <kraj9380286@gmail.com>
2025-05-09 20:01:33 +0530

    commited

--- Bacis/Switch/switch.cpp
+++ Bacis/Switch/switch.cpp
@@ -1,27 +1,7 @@
-#include <stdio.h>
-
-int main() {
-    int num;
-    printf("value of num: ");
-    scanf("%d", &num);
-
-    switch (num > 0){
-        case 1:
-        
-        printf("num is positive");
-        break;
-        case 0:
-        switch(num<0){
-            
-            case 1:
-            printf("num is negative");
-            break;
-            case 0:
-            printf("num is zero");
-            break;
-        }
-        break;
-
-    }
-       
+#include<iostream>
+using namespace std;
+int main(){
+    int a=5;
+    int*ch=&a;
+    cout<<ch<<endl;
 }
