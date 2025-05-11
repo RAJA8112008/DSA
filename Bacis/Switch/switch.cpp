2138a9041e2e23f4085aab436f59a3ffc1010da7
Raja <kraj9380286@gmail.com>
2025-05-11 13:02:09 +0530

    commited

--- Bacis/Switch/switch.cpp
+++ Bacis/Switch/switch.cpp
@@ -4,4 +4,10 @@ int main(){
   int a=5;
   int*p=&a;
   int**q=&p;
+  cout<<a<<endl;
+  cout<<"Adress of a"<<&a<<endl;
+  cout<<"address of p"<<p<<endl;
+  cout<<"address of p"<<&p<<endl;
+  cout<<"adress of q"<<q<<endl;
+  cout<<&q<<endl;
 }
