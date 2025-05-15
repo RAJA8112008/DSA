c04aabab9fa543582b3e0270aee46993cda41e4b
Raja <kraj9380286@gmail.com>
2025-05-15 13:15:07 +0530

    commited

--- Bacis/Switch/BinarySearch.cpp
+++ Bacis/Switch/BinarySearch.cpp
@@ -1,12 +1,12 @@
 #include<stdio.h>
 int main(){
-    int num,rem,sum=0;
+    int num,rem,val,sum=0;
     printf("num:");
     scanf("%d",&num);
     while(num!=0){
-        num=num%10;
+        rem=num%10;
         sum=sum*10+rem;
-        num=num/10;
+        val=num/10;
     }
-    printf("\nnum:%d",sum);
+    printf("\nnum:%d",num);
 }
