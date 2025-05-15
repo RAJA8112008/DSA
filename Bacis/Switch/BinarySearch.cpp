b7fa2540598b41e45ebdd70ce3f73e58baf3dbec
Raja <kraj9380286@gmail.com>
2025-05-15 13:13:05 +0530

    commited

--- Bacis/Switch/BinarySearch.cpp
+++ Bacis/Switch/BinarySearch.cpp
@@ -1,29 +1,12 @@
-#include<iostream>
-using namespace std;
-
-int firstOccurrence(int arr[], int n, int key) {
-    int start = 0;
-    int end = n - 1;
-    int ans = -1;
-
-    while (start <= end) {
-        int mid = start + (end - start) / 2;  // ✅ Moved inside loop
-        if (arr[mid] == key) {
-            ans = mid;
-            end = mid - 1;  // move left for first occurrence
-        } else if (key > arr[mid]) {
-            start = mid + 1;
-            
-        } else {
-            end = mid - 1;
-        }
+#include<stdio.h>
+int main(){
+    int num,rem,sum=0;
+    printf("num:");
+    scanf("%d",&num);
+    while(num!=0){
+        num=num%10;
+        sum=sum*10+rem;
+        num=num/10;
     }
-    return ans;
-}
-
-int main() {
-    int Binaryarr[6] = {1, 2, 3, 4, 4, 5};
-    int result = firstOccurrence(Binaryarr, 6, 4);
-    cout << "index of 4: " << result << endl;
-    return 0;
+    printf("\nnum:%d",sum);
 }
