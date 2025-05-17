a6e2920835fe5d58f7c7bcc1c62fc22bc3256965
Raja <kraj9380286@gmail.com>
2025-05-17 21:33:57 +0530

    commited

--- Bacis/Switch/collegeassi.c/printprimenum.c
+++ Bacis/Switch/collegeassi.c/printprimenum.c
@@ -1,4 +1,15 @@
 #include<stdio.h>
 int main(){
-    
+    int first,Last;
+    //start num
+    printf("Enter the First num:");
+    scanf("%d",&first);
+    //last num
+    printf("Enter the Last num:");
+    scanf("%d",&Last);
+    for(int i=first;i<=Last;i++){
+        if(first%i==0){
+            printf("Prime Num :%d",first);
+        }
+    }
 }
