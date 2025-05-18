a0d0980daf3a8dfe43b015ce39c92d6602a23241
Raja <kraj9380286@gmail.com>
2025-05-18 21:31:52 +0530

    commited

--- Bacis/Switch/collegeassi.c/factorial.c
+++ Bacis/Switch/collegeassi.c/factorial.c
@@ -1,4 +1,34 @@
 #include<stdio.h>
 int main(){
-    
+    int i,num,factorial=1;
+    printf("Enter the num:");
+    scanf("%d",&num);
+if(num<0){
+    printf("\nNOT possible %d",num);
 }
+else{
+for(i=1;i<=num;i++){
+    factorial*=i;   
+  }
+  printf("\nFactorial is %d:%d",num,factorial);
+}
+return 0;
+}
+// #include <stdio.h>
+
+// int main() {
+//     int i, num, factorial = 1;
+//     printf("Enter the num: ");
+//     scanf("%d", &num);
+
+//     if (num < 0) {
+//         printf("\nNOT possible for negative number: %d", num);
+//     } else {
+//         for (i = 1; i <= num; i++) {
+//             factorial *= i;
+//         }
+//         printf("\nFactorial of %d is: %d", num, factorial);
+//     }
+
+//     return 0;
+// }
