e4be5edff7ad0626c0bcd9508a196b7f483ed30d
Raja <kraj9380286@gmail.com>
2025-05-28 00:06:31 +0530

    commited

--- Bacis/Switch/collegeassi.c/exam.c
+++ Bacis/Switch/collegeassi.c/exam.c
@@ -1,18 +1,13 @@
 #include<stdio.h>
 void main(){
-//reverse the num
-int num,rem,temp,rev=0;
-printf("Enter the value:");
+    int rem,num,sum=0;
+
+printf("Enter the num:");
 scanf("%d",&num);
-temp=num;
- while(num>0){
-   rem=num%10;
-  rev=rev+rem*rem*rem;
-  num=num/10;
- }
- if(temp==rev){
-    printf("Num is armstrome");
- }else{
-    printf("num is not armstome");
- }
+while(num!=0){
+    rem=num%10;
+    sum=sum+rem;
+    num=num/10;
+}
+printf("Total sum:%d",sum);
 }
