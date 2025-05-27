339dae36fe3cf574ed7ebe867e848f0db45aabf3
Raja <kraj9380286@gmail.com>
2025-05-27 18:22:07 +0530

    commited

--- Bacis/Switch/collegeassi.c/exam.c
+++ Bacis/Switch/collegeassi.c/exam.c
@@ -1,11 +1,20 @@
 #include<stdio.h>
 void main(){
-    int arr[5],i;
-    for(i=0;i<5;i++){
-        scanf("%d",&arr[i]);
+    int arr[4][4],i,j;
+    printf("Enter the values");
+    for(i=0;i<4;i++){
+    scanf("%d",&arr[i]);
+    for(j=0;j<4;j++){
+        scanf("%d",arr[j]);
     }
-    printf("Array is :");
-    for(i=0;i<5;i++){
-        printf(" %d",arr[i]);
     }
+    printf("Array is");
+    for(i=0;i<4;i++){
+    printf("%d\t",arr[i]);
+    for(j=0;j<4;j++){
+        printf("%d\t",arr[j]);
+    }
+    }
+
+
 }
