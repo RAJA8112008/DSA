7a6fa05cdf66b07266d4e8c4d5f63946923604a6
Raja <kraj9380286@gmail.com>
2025-05-27 19:25:21 +0530

    commited

--- Bacis/Switch/collegeassi.c/exam.c
+++ Bacis/Switch/collegeassi.c/exam.c
@@ -21,7 +21,7 @@ void main(){
     printf("\n");
     for(i=0;i<3;i++){
         for(j=0;j<3;j++){
-            printf("%d\t",&arr[i][j]);
+            printf("%d\t",arr[i][j]);
         }
         printf("\n");
     }
