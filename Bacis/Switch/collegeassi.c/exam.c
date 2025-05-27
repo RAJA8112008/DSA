345681eadea6ad77c3860d33294e2d3d6564bca9
Raja <kraj9380286@gmail.com>
2025-05-27 23:39:26 +0530

    commited

--- Bacis/Switch/collegeassi.c/exam.c
+++ Bacis/Switch/collegeassi.c/exam.c
@@ -1,46 +1,13 @@
 #include<stdio.h>
 void main(){
-    int arr[3][3],brr[3][3],crr[3][3],i,j,k;
-    printf("Enter the values:");
-    for(i=0;i<3;i++){
-        for(j=0;j<3;j++){
-            scanf("%d",&arr[i][j]);
-        }
-    }
-    for(i=0;i<3;i++){
-        for(j=0;j<3;j++){
-            scanf("%d",&brr[i][j]);
-        }
-    }
-    for(i=0;i<3;i++){  
-        for(j=0;j<3;j++){
-            crr[i][j]=0;
-            for(k=0;k<3;k++){
-      crr[i][j]=crr[i][j]+arr[i][k]*brr[k][j];
-        }
-    }
-}
-     printf("first matrix:\n");
-    for(i=0;i<3;i++){
-        for(j=0;j<3;j++){
-            printf("%d\t",arr[i][j]);
-        }
-        printf("\n");
-    }
-     printf("second matrix:\n");
-   
-    for(i=0;i<3;i++){
-        for(j=0;j<3;j++){
-            printf("%d\t",brr[i][j]);
-        }
-      printf("\n");
-    }
-    printf("sum of matrix:\n");
-    for(i=0;i<3;i++){
-         crr[i][j]=arr[i][j]+brr[i][j];
-        for(j=0;j<3;j++){
-            printf("%d\t",crr[i][j]);
-        }
-         printf("\n");
-    }
+//reverse the num
+int num,rem,rev=0;
+printf("Enter the value:");
+scanf("%d",&num);
+ while(num>0){
+   rem=num%10;
+  rev=rev*10+rem;
+  num=num/10;
+ }
+ printf("reverse num:%d\n",rev);
 }
