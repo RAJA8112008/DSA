fd9639705a915dd08db95a79cccfbb2293270844
Raja <kraj9380286@gmail.com>
2025-05-27 17:51:56 +0530

    commited

--- Bacis/Switch/collegeassi.c/exam.c
+++ Bacis/Switch/collegeassi.c/exam.c
@@ -3,9 +3,8 @@ void main(){
     int  i,num,fact=1;
     printf("Enter the num:");
     scanf("num",&num);
-    for(i=1;i<num;i++){
+    for(i=1;i<=num;i++){
         fact*=i;
     }
-    printf("Fact:%d",num);
-
+    printf("Fact:%d",num,fact);
 }
