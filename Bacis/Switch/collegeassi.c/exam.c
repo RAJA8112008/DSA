c2d4e599af77c7b86bf9d7824aae90e4742c20b3
Raja <kraj9380286@gmail.com>
2025-05-27 18:38:10 +0530

    commited

--- Bacis/Switch/collegeassi.c/exam.c
+++ Bacis/Switch/collegeassi.c/exam.c
@@ -7,6 +7,8 @@ void main(){
         scanf("%d",&arr[i][j]);
     }
     }
+    printf("arr is:");
+    printf("\n");
     for(i=0;i<4;i++){
     for(j=0;j<4;j++){
         printf("%d\t",arr[i][j]);
