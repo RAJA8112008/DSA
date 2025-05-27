6126bde38ef1ad1f997b906c9196171ff71cac57
Raja <kraj9380286@gmail.com>
2025-05-27 19:30:19 +0530

    commited

--- Bacis/Switch/collegeassi.c/exam.c
+++ Bacis/Switch/collegeassi.c/exam.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 void main(){
-    int arr[3][3],brr[3][3],crr[3][3],i,j;
+    int arr[3][3],brr[3][3],crr[3][3],i,j,k;
     printf("Enter the values:");
     for(i=0;i<3;i++){
         for(j=0;j<3;j++){
@@ -14,9 +14,12 @@ void main(){
     }
     for(i=0;i<3;i++){  
         for(j=0;j<3;j++){
-      crr[i][j]=arr[i][j]+brr[i][j];
+            crr[i][j]=0;
+            for(k=0;k<3;k++){
+      crr[i][j]=crr[i][j]+arr[i][k]*brr[k][j];
         }
     }
+}
      printf("first matrix:\n");
     for(i=0;i<3;i++){
         for(j=0;j<3;j++){
