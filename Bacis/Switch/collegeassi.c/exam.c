161d8c7ffff3d2ece5c527e3286ee30f1c7169f6
Raja <kraj9380286@gmail.com>
2025-05-27 18:15:42 +0530

    commited

--- Bacis/Switch/collegeassi.c/exam.c
+++ Bacis/Switch/collegeassi.c/exam.c
@@ -1,18 +1,11 @@
 #include<stdio.h>
 void main(){
-    int num,i;
-    int count=0;
-    printf("Enter the num:");
-    scanf("%d",&num);
-    for(int i=1;i<=num;i++){
-    if(num%i==0){
-       count++;
+    int arr[5],i;
+    for(i=0;i<5;i++){
+        scanf("%d",&arr[i]);
     }
-}
-if(count==2){
-    printf("Num is prime",num);
-}else{
-    printf("num is not prime",num);
-}
-
+    printf("Array is :");
+    for(i=0;i<5;i++){
+        printf(" %d",arr[i]);
+    }
 }
