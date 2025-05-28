108f0cf7d2ef11140dc5ee5314c95faff9625381
Raja <kraj9380286@gmail.com>
2025-05-28 07:53:15 +0530

    commited

--- Bacis/Switch/collegeassi.c/exam.c
+++ Bacis/Switch/collegeassi.c/exam.c
@@ -1,18 +1,25 @@
 #include<stdio.h>
 void main(){
-    int arr[3][3],i,j;
-    printf("Enter the values:");
+    int arr[10][10],i,j;
+    printf("Enter the values:\n");
     for(i=0;i<3;i++){
         for(j=0;j<3;j++){
             scanf("%d",&arr[i][j]);
         }
     }
-    printf("array is:\n ");
+    printf("Matrix is:\n");
     for(i=0;i<3;i++){
         for(j=0;j<3;j++){
             printf("%d\t",arr[i][j]);
         }
         printf("\n");
     }
-
+    printf("transpose matrix is:\n");
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            printf("%d\t",arr[j][i]);
+        }
+        printf("\n");
+    }
+    
 }
