726fea1ea8aa92779fd1019321da3b0f3c15fe85
Raja <kraj9380286@gmail.com>
2025-05-28 07:28:32 +0530

    commited

--- Bacis/Switch/collegeassi.c/exam.c
+++ Bacis/Switch/collegeassi.c/exam.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 void main(){
-    int num,i,factorial=1;
+    int num,i;
+    unsigned long factorial=1;
     printf("Enter the num:");
     scanf("%d",&num);
     for(i=1;i<=num;i++){
         factorial*=i;
     }
-printf("factorial:%d",factorial);
+printf("factorial is %lu",factorial);
 }
