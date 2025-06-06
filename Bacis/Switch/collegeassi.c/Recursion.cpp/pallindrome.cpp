356fb726dc88fed88539f7dbc9f2a03cdf691ff9
Raja <kraj9380286@gmail.com>
2025-06-06 20:20:39 +0530

    commited

--- Bacis/Switch/collegeassi.c/Recursion.cpp/pallindrome.cpp
+++ Bacis/Switch/collegeassi.c/Recursion.cpp/pallindrome.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
-int checkpallindrome( string str,int s,int e){
+bool checkpallindrome( string str,int s,int e){
  //base condition
  if(s>=e){
     return true;
@@ -8,21 +8,20 @@ int checkpallindrome( string str,int s,int e){
  // solve one case
  if(str[s]!=str[e]){
     return false;
- }else{
-    return true;
  }
  return checkpallindrome(str,s+1,e-1);
  }
 
 int main(){
-    string str="nitin";
-    int s=0;
-    int e=str.length()-1;
-    for(auto ch:str){
-        if(ch<'A' && ch>='Z'){
+    string str="Nitin";
+     for(auto&ch:str){
+        if(ch>='A' && ch<='Z'){
             ch=ch+32;
         }
     }
+    int s=0;
+    int e=str.length()-1;
+   
   if(checkpallindrome(str,s,e)){
     cout<<"Pallindrome"<<endl;
   }else{
