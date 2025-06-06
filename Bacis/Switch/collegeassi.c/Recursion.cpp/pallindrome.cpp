cce022bbce330bfccd8c1437bf71de3439c98208
Raja <kraj9380286@gmail.com>
2025-06-06 19:36:26 +0530

    commited

--- Bacis/Switch/collegeassi.c/Recursion.cpp/pallindrome.cpp
+++ Bacis/Switch/collegeassi.c/Recursion.cpp/pallindrome.cpp
@@ -1,6 +1,24 @@
 #include<iostream>
 using namespace std;
+
+int checkpallindrome(int s,int e){
+
+ //base condition
+ if(s>e){
+    return false;
+ }
+ // solve one case
+ if(s==e){
+    return true;
+ }else{
+    return false;
+ }
+ return checkpallindrome(s+1,e-1);
+ }
+
 int main(){
     string str="raja";
-   
+    int s=str[0];
+    int e=str.length();
+   return checkpallindrome(s,e);
 }
