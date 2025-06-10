7e199ef6219cc6fbaf6472aa9170c4f0dfed93eb
Raja <kraj9380286@gmail.com>
2025-06-10 22:16:27 +0530

    commited

---
+++ Bacis/Switch/collegeassi.c/pattern.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int rows = 5;
+
+    // Upper part of the tree
+    for (int i = 0; i < rows - 1; i++) {
+        for (int j = 0; j < rows - i - 1; j++) {
+            printf("  ");
+        }
+        printf("*");
+        if (i > 0) {
+            for (int j = 0; j < 2 * i - 1; j++) {
+                printf("  ");
+            }
+            printf(" *");
+        }
+        printf("\n");
+    }
+
+    // Bottom row (full stars)
+    for (int i = 0; i < 2 * rows - 1; i++) {
+        printf("* ");
+    }
+    printf("\n");
+
+    return 0;
+}
