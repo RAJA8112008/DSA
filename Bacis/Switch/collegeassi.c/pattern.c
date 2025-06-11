ea14bf92c97983aa659428550583e4f30b48145e
Raja <kraj9380286@gmail.com>
2025-06-12 02:10:07 +0530

    commited

--- Bacis/Switch/collegeassi.c/pattern.c
+++ Bacis/Switch/collegeassi.c/pattern.c
@@ -1,18 +1,19 @@
-#include <stdio.h>
-
-int gcd(int a, int b) {
-    if (b == 0)
-        return a;
-    return gcd(b, a % b);
+#include<stdio.h>
+int ftarget(int arr[],int target,int n){
+for(int i=0;i<n;i++){
+    if(arr[i]==target){
+        return i;
+    }
 }
-
-int main() {
-    int num1, num2;
-    printf("Enter two numbers: ");
-    scanf("%d %d", &num1, &num2);
-
-    int result = gcd(num1, num2);
-    printf("GCD of %d and %d is %d\n", num1, num2, result);
-
-    return 0;
+}
+int main(){
+    int arr[5]={10,25,46,89,5};
+    int target=89;
+    int n=sizeof(arr)/sizeof(arr[0]);
+    int ans=ftarget(arr,target,n);
+    if(target!=-1){
+    printf("index of target %d\n",ans);
+    }else{
+        printf("Target is not available:\n",ans);
+    }
 }
