f9ce4f3741f04a519b870eef88d23e01f13b0998
Raja <kraj9380286@gmail.com>
2025-06-12 01:33:44 +0530

    commited

--- Bacis/Switch/collegeassi.c/pattern.c
+++ Bacis/Switch/collegeassi.c/pattern.c
@@ -3,13 +3,13 @@ int fact(int num){
     if(num==0){
         return 1;
     }
-    return fact(num-1);
+    return num*fact(num-1);
 }
 
 int main(){
     int num;
     printf("Enter the num:");
     scanf("%d",&num);
-    return fact(num);
-    printf("%d",num,fact(num));
+    int result=fact(num);
+    printf(" Factorial of %d is %d\n",num,result);
 }
