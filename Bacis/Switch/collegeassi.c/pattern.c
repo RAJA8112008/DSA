d460311a7e6599226bedb02e4e07b4216063d5d6
Raja <kraj9380286@gmail.com>
2025-06-12 02:55:28 +0530

    commited

--- Bacis/Switch/collegeassi.c/pattern.c
+++ Bacis/Switch/collegeassi.c/pattern.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
-int bubble(int arr[],int n){
+void bubble(int arr[],int n){
     for(int i=0;i<n-1;i++){
-        for(int j=i+1;j<n-i-1;j++){
+        for(int j=0;j<n-i-1;j++){
             if(arr[j]<arr[j+1]){
                 int temp=arr[j];
                  arr[j]=arr[j+1];
@@ -16,6 +16,7 @@ int  main(){
   bubble(arr,n);
   printf("Sorted array:\n");
   for(int i=0;i<n;i++){
-    printf("ARRAY is :%d\n",arr[i]);
+    printf("%d",arr[i]);
   }
+  printf("\n");
 }
