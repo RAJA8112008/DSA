c9fdd15768941ef8909bb376060a1e6f8db479f9
Raja <kraj9380286@gmail.com>
2025-06-18 16:30:52 +0530

    commited

--- pattern.cpp
+++ pattern.cpp
@@ -1,50 +1,19 @@
 #include<iostream>
 using namespace std;
-
-void merge(int*arr,int left,int mid,int right){
-    int n1=mid-left+1;
-    int n2=right-mid;
-    int L[n1],R[n2];
-    for(int i=0;i<n1;i++){
-        L[i]=arr[left+i];
-      }
-        for(int j=0;j<n2;j++){
-            R[j]=arr[mid+1+j];
-        }
-        //merge two arrays
-       int i=0,j=0,k=left;
-       while(i<n1 && j<n2){
-        if(L[i]<R[j]){
-            arr[k++]=L[i++];
-        }else{
-            arr[k++]=R[j++];
-        }
-        //coping all the remining elements
-        while(i<n1){
-            arr[k++]=L[i++];
-        }
-        while(j<n2){
-            arr[k++]=R[j++];
-        }
-       }
-    }
-void mergSort(int*arr,int left,int right){
-    if(left>=right){
-        return;
-    }
-    int mid =left+(right-left)/2;
-    mergSort(arr,left,mid);
-    mergSort(arr,mid+1,right);
-    merge(arr,left,mid,right);
+ void merge(int*arr,int st,int mid,int end){
+    
+ }
+void mergeSort(int*arr,int st,int end){
+    int mid=st+(end-st)/2;
+    mergeSort(arr,st,mid);
+    mergeSort(arr,mid+1,end);
+    merge(arr,st,mid,end);
 }
 
 
 int main(){
-    int arr[]={2,2,2,2,6,7,8,6,4,7,8,54,8,89,79};
+    int arr[]={1,2,4,0,57,51,889,47,12,11,23};
     int n=sizeof(arr)/sizeof(arr[0]);
-    mergSort(arr,0,n-1); 
-    for(int i=0;i<n;i++){
-        cout<<arr[i]<<" ";
-    }
-    cout<<endl;
+    mergeSort(arr,0,n-1);
+
 }
