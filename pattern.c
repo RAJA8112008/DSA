d16c9fdafae36633708f7adde4696f5cb1e2d96a
Raja <kraj9380286@gmail.com>
2025-06-23 16:45:09 +0530

    commited

---
+++ pattern.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+   void main(){
+    int arr[5],i,n,j,temp;
+    printf("Enter the values/n:");
+     scanf("%d",&n);
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    //buble sorting 
+    for(i=0;i<n;i++){
+        for(j=0;j<n-i+1;j++){
+            if(arr[j]>arr[j+1]){
+                temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+    printf("Sorted Array:/n");
+    for(int i=0;i<n;i++){
+        printf("%d",arr[i]);
+    }
+   }
