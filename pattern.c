9560c0216c42b8969055e355a952291320ce1197
Raja <kraj9380286@gmail.com>
2025-06-23 18:27:27 +0530

    commited

--- pattern.c
+++ pattern.c
@@ -1,25 +1,29 @@
 #include<stdio.h>
-void main(){
-    int arr[10][10],i,j;
-    printf("Enter the values:\n");
-    for(i=0;i<3;i++){
-        for(j=0;j<4;j++){
-            scanf("%d",&arr[i][j]);
-        }
+void main (){
+int arr[10][10],i,j,row,col;
+printf("Enter the row:");
+scanf("%d", &row);
+printf("Enter the col:");
+scanf("%d", &col);
+printf("Enter the values:\n");
+for(i=0;i<row;i++){
+    for(j=0;j<col;j++){
+        scanf("%d", &arr[i][j]);
     }
-    printf("Matrix is :\n");
-    for(i=0;i<3;i++){
-        for(j=0;j<4;j++){
-            printf("%d\t",arr[i][j]);
-        }
-        printf("\n");
-        
+}
+printf("Matrix is:\n");
+for(i=0;i<row;i++){
+    for(j=0;j<col;j++){
+        printf("%d\t",arr[i][j]);
     }
-    printf("Transpose:\n");
-    for(i=0;i<4;i++){
-        for(j=0;j<3;j++){
-            printf("%d\t",arr[j][i]);
-        }
-        printf("\n");
+    printf("\n");
+}
+ printf("Transpose matrix:\n");
+ for(i=0;i<col;i++){
+    for(j=0;j<row;j++){
+        printf("%d\t",arr[j][i]);
     }
+    printf("\n");
+ }
+
 }
