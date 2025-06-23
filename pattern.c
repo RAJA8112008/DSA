10e1443fc44d84f1b8d803793dccc5131d7e47c7
Raja <kraj9380286@gmail.com>
2025-06-23 19:13:30 +0530

    commited

--- pattern.c
+++ pattern.c
@@ -1,45 +1,43 @@
 #include<stdio.h>
 void main(){
-    int arr[2][2],brr[2][2],bcc[2][2],i,j,k;
-    printf("Enter the valus of matrix1:\n");
-    for(i=0;i<2;i++){
-        for(j=0;j<2;j++){
+    int arr[3][3],brr[3][3],crr[3][3],i,j;
+    printf(" Enter matrix 1:\n");
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
             scanf("%d",&arr[i][j]);
         }
     }
-    printf("matrix 1 is :\n");
-    for(i=0;i<2;i++){
-        for(j=0;j<2;j++){
-            printf("%d\t",arr[i][j]);
-        }
-          printf("\n");
-    }
-     printf("Enter the valus of matrix2:\n");
-    for(i=0;i<2;i++){
-        for(j=0;j<2;j++){
+     printf(" Enter matrix2:\n");
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
             scanf("%d",&brr[i][j]);
         }
     }
-    printf("matrix 2 is :\n");
-    for(i=0;i<2;i++){
-        for(j=0;j<2;j++){
-            printf("%d\t",brr[i][j]);
-        }
-          printf("\n");
-    }
-    printf("Multiply matrix:\n");
-    for(i=0;i<2;i++){
-        for(j=0;j<2;j++){
-            bcc[i][j]=0;
-            for(k=0;k<2;k++){
-                bcc[i][j]= bcc[i][j]+arr[i][k]*brr[k][j];
-            }
-        }
-    }
-    for(i=0;i<2;i++){
-        for(j=0;j<2;j++){
-            printf("%d\t",bcc[i][j]);
+     printf("matrix 1 is :\n");
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            printf("%d\t",arr[i][j]);
         }
         printf("\n");
     }
+ printf("matrix 2 is :\n");
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            printf("%d\t",brr[i][j]);
+        }
+        printf("\n");
+    }
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            crr[i][j]=arr[i][j]+brr[i][j];
+        }
+    }
+    printf(" Addition matrix  is :\n");
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            printf("%d\t",crr[i][j]);
+        }
+        printf("\n");
+    }
+
 }
