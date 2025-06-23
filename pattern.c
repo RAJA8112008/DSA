b59289aa95730a86a4cbb2ac415ad2d62675f052
Raja <kraj9380286@gmail.com>
2025-06-23 16:54:02 +0530

    commited

--- pattern.c
+++ pattern.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
    void main(){
     int arr[5],i,n,j,temp;
-    printf("Enter the values/n:");
+    printf("Enter the values\n:");
      scanf("%d",&n);
     for(i=0;i<n;i++){
         scanf("%d",&arr[i]);
     }
     //buble sorting 
     for(i=0;i<n;i++){
-        for(j=0;j<n-i+1;j++){
+        for(j=0;j<n-i-1;j++){
             if(arr[j]>arr[j+1]){
                 temp=arr[j];
                 arr[j]=arr[j+1];
@@ -20,5 +20,5 @@
     for(int i=0;i<n;i++){
         printf("%d",arr[i]);
     }
-    
+  printf("\n");
    }
