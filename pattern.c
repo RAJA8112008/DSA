7b16b65b4da79716c36e9f7c00e29fd7272dbd8c
Raja <kraj9380286@gmail.com>
2025-06-24 16:31:08 +0530

    commited

--- pattern.c
+++ pattern.c
@@ -1,18 +1,16 @@
 #include<stdio.h>
-void max(int,int);
 int main(){
-   int a,b;
-    printf("Enter the values:");
+    int a,b;
+    printf("Enter the nums:");
     scanf("%d",&a);
     scanf("%d",&b);
-    max(a,b);
+    swap(a,b);
 }
-  void max(int a,int b){
-     if(a>b){
-        printf("A is greater");
-     }else{
-         printf("B is greater");
-     }
-
-    
+void swap(int x,int y){
+    int temp;
+     temp=x;
+     x=y;
+     y=temp;
+     printf("Num is:%d %d",x,y);
+     
 }
