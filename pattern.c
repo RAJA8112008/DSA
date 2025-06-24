5c5d316a22b553bd0f061a29a40f7e92bab9a5b8
Raja <kraj9380286@gmail.com>
2025-06-24 23:53:01 +0530

    commited

--- pattern.c
+++ pattern.c
@@ -1,29 +1,11 @@
 #include<stdio.h>
-int main(){
-    char op;
-    int a,b;
-    printf("Enter the value  :");
-    scanf("%d",&a);
-    scanf("%d",&b);
-    scanf(" %c",&op);
-    printf("%c",op);
-    switch(op){
-        case '+':
-        printf("sum=%d",a+b);
-        break;
-         case '-':
-        printf("sub=%d",a-b);
-        break;
-         case '*':
-        printf("Multip=%d",a*b);
-        break;
-         case '/':
-        printf("subtraction=%d",a/b);
-        break;
-         case '%':
-        printf("modul=%d",a%b);
-        break;
-        default:
-        printf("Kuch nhi");
+void main(){
+    int year;
+    printf("Enter the year:");
+    scanf("%d",&year);
+    if((year%100!=0 && year%400==0)||(year%4==0)){
+        printf("Year is leap");
+    }else{
+        printf("year is not leap");
     }
 }
