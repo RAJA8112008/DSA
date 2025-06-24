6968900d4ecab926ed9747dad7a60f2ba77e2f41
Raja <kraj9380286@gmail.com>
2025-06-24 23:44:43 +0530

    commited

--- pattern.c
+++ pattern.c
@@ -1,36 +1,29 @@
 #include<stdio.h>
-void main(){
-    int arr[10],i,n,j,s;
-    int st,ed,mid,count=0;
-    printf("Enter the value of n:");
-    scanf("%d",&n);
-    for(i=0;i<n;i++){
-        scanf("%d",&arr[i]);
+int main(){
+    char op;
+    int a,b;
+    printf("Enter the value  :");
+    scanf("%d",&a);
+    scanf("%d",&b);
+    scanf(" %c",&op);
+    printf("%c",op);
+    switch(op){
+        case '+':
+        printf("sum=%d",a+b);
+        break;
+         case '-':
+        printf("sub=%d",a-b);
+        break;
+         case '*':
+        printf("Multip=%d",a*b);
+        break;
+         case '/':
+        printf("subtraction=%d",a/b);
+        break;
+         case '%':
+        printf("modul=%d",a%b);
+        break;
+        default:
+        printf("Kuch nhi");
     }
-    printf("Array is:");
-    for(i=0;i<n;i++){
-        printf("%d",arr[i]);
-    }
-    printf("\n");
-    printf("Searching Element:");
-    scanf("%d",&s);
-    st=0;
-    ed=n-1;
-    while(st<=ed){
-        mid=st+(ed-st)/2;
-        if(arr[mid]==s){
-            count=1;
-            break;
-        }else if(s<arr[mid]){
-            ed=mid-1;
-        }else{
-            st=mid+1;
-        }
-    }
-    if(count==1){
-        printf("Available");
-    }else{
-        printf("not Available");
-    }
-    
 }
