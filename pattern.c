137c4c4535a1c229d8ecfd40ede236dc70a335e1
Raja <kraj9380286@gmail.com>
2025-06-24 17:50:17 +0530

    commited

--- pattern.c
+++ pattern.c
@@ -1,30 +1,28 @@
 #include<stdio.h>
 void main(){
-    int arr[100],i,j,n,temp;
+    int temp,n,i,j,arr[100];
     printf("Enter the value of n:");
     scanf("%d",&n);
-    for(int i=0;i<n;i++){
+    for(i=0;i<n;i++){
         scanf("%d",&arr[i]);
     }
-    printf("Array is :");
-    for(int i=0;i<n;i++){
-       printf("%d ",arr[i]);
-    }
-    printf("\n");
-    //buble sort process
-    printf("Sorted Array:");
-    for(i=0;i<n;i++){
-      for(j=0;j<n-i-1;j++){
-        if(arr[j]>arr[j+1]){
-            temp=arr[j];
-           arr[j]=arr[j+1];
-            arr[j+1]=temp;
-        }
-      }
-    }
-    for(i=0;i<n;i++){
+    printf("Original array:");
+   for(i=0;i<n;i++){
         printf("%d ",arr[i]);
     }
+    //selection sort 
+    for(i=0;i<n;i++){
+        for(j=i+1;j<n;j++){
+            if(arr[i]>arr[j]){
+                temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+    }
     printf("\n");
+    printf("Sorted array:");
+   for(i=0;i<n;i++){
+        printf("%d ",arr[i]);
+    }
 }
-
