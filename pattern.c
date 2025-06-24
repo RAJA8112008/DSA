874789f0b2593c9b8a622e67b1a639f37f701158
Raja <kraj9380286@gmail.com>
2025-06-24 19:44:21 +0530

    commited

--- pattern.c
+++ pattern.c
@@ -1,29 +1,36 @@
 #include<stdio.h>
 void main(){
-    int n,arr[100],j,i,x,count=0;
+    int arr[10],i,n,j,s;
+    int st,ed,mid,count=0;
     printf("Enter the value of n:");
     scanf("%d",&n);
     for(i=0;i<n;i++){
         scanf("%d",&arr[i]);
     }
-    printf("Array is :");
+    printf("Array is:");
     for(i=0;i<n;i++){
-        printf("%d ",arr[i]);
+        printf("%d",arr[i]);
     }
     printf("\n");
-    printf("Finder element:");
-    scanf("%d",&x);
-    // finding element in array 
-    for(i=0;i<n;i++){
-    if(arr[i]==x){
-       count=1;
-       break;
+    printf("Searching Element:");
+    scanf("%d",&s);
+    st=0;
+    ed=n-1;
+    while(st<=ed){
+        mid=st+(ed-st)/2;
+        if(arr[mid]==s){
+            count=1;
+            break;
+        }else if(s<arr[mid]){
+            ed=mid-1;
+        }else{
+            st=mid+1;
+        }
     }
+    if(count==1){
+        printf("Available");
+    }else{
+        printf("not Available");
+    }
+    
 }
-if(count=1){
-printf("Available ");
-}else{
-   printf(" not Available"); 
-}
-}
-
