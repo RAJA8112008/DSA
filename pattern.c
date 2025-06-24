a98af2f1a9f6d72306500c7af3f520f5e09372f9
Raja <kraj9380286@gmail.com>
2025-06-24 11:06:00 +0530

    commited

--- pattern.c
+++ pattern.c
@@ -1,23 +1,15 @@
 #include<stdio.h>
-void main(){
-    int marks;
-    printf("Enter the value of marks:");
-    scanf("%d",&marks);
-    switch(marks/10){
-        case 10:
-        case 9:
-        printf("G-A");
-        break;
-        case 8:
-        printf("G-B");
-        break;
-        case 7:
-        printf("G-C");
-        break;
-        case 6:
-        printf("G-D");
-        break;
-        default:
-        printf("Fail");
-    }
+
+int sum(int num){
+    if(num==0){
+        return 0;
+    } 
+   return num+sum(num-1);
+
+}
+int  main(){
+    int n;
+    scanf("%d",&n);
+   int result=sum(n);
+   printf("%d",result);
 }
