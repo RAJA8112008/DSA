12d5ae36936858721c700def42da7a7766c5fd9b
Raja <kraj9380286@gmail.com>
2025-06-24 17:25:49 +0530

    commited

--- pattern.c
+++ pattern.c
@@ -1,16 +1,30 @@
 #include<stdio.h>
-//pass by reference
-int main(){
-    int temp,a,b;
-    printf("Enter the value :");
-    scanf("%d",&a);
-    scanf("%d",&b);
-    swap(&a,&b);
-    printf("swep num:%d%d",a,b);
+void main(){
+    int arr[100],i,j,n,temp;
+    printf("Enter the value of n:");
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    printf("Array is :");
+    for(int i=0;i<n;i++){
+       printf("%d ",arr[i]);
+    }
+    printf("\n");
+    //buble sort process
+    printf("Sorted Array:");
+    for(i=0;i<n;i++){
+      for(j=0;j<n-i-1;j++){
+        if(arr[j]>arr[j+1]){
+            temp=arr[j];
+           arr[j]=arr[j+1];
+            arr[j+1]=temp;
+        }
+      }
+    }
+    for(i=0;i<n;i++){
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
 }
-void swap(int *x,int *y){
-   int temp;
-    temp=*x;
-    *x=*y;
-    *y=temp;
-}
+
