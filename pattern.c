601b2064e2a114fc737055057f1533b3e9c740bc
Raja <kraj9380286@gmail.com>
2025-06-24 11:22:55 +0530

    commited

--- pattern.c
+++ pattern.c
@@ -1,15 +1,22 @@
 #include<stdio.h>
 
 int fact(int num){
-    if(num==1){
+    if(num==0||num==1){
         return 1;
     }
     return num*fact(num-1);
 }
+
 int  main(){
     int n;
 printf("Enter the num:");
 scanf("%d",&n);
-int result=fact(n);
+if(n<0){
+
+    printf("NO number found");
+}else{
+ int result=fact(n);
 printf("%d",result);
+}
+return 0;
 }
