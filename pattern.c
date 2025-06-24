1a8fc9847cba2c3de8f23c028df81b537a263ea3
Raja <kraj9380286@gmail.com>
2025-06-24 18:33:23 +0530

    commited

--- pattern.c
+++ pattern.c
@@ -1,28 +1,27 @@
 #include<stdio.h>
 void main(){
-    int temp,n,i,j,arr[100];
+    int i,n,key,j,arr[100];
     printf("Enter the value of n:");
     scanf("%d",&n);
     for(i=0;i<n;i++){
         scanf("%d",&arr[i]);
     }
-    printf("Original array:");
-   for(i=0;i<n;i++){
-        printf("%d ",arr[i]);
-    }
-    //selection sort 
+    printf("array is:");
     for(i=0;i<n;i++){
-        for(j=i+1;j<n;j++){
-            if(arr[i]>arr[j]){
-                temp=arr[i];
-                arr[i]=arr[j];
-                arr[j]=temp;
-            }
-        }
+        printf("%d",arr[i]);
     }
-    printf("\n");
-    printf("Sorted array:");
-   for(i=0;i<n;i++){
-        printf("%d ",arr[i]);
+    // insertion algorithum
+    for(i=1;i<n;i++){
+        key=arr[i];
+        j=i-1;
+        while(j>=0 && arr[i]>key){
+            arr[j+1]=arr[j];
+            j=j-1;
+        }
+        arr[j+1]=key;
+    }
+    printf("Sorted array");
+    for(i=0;i<n;i++){
+        printf("%d",arr[i]);
     }
 }
