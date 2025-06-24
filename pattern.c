85005be8ec652003419646d57e9856a6d796ac5a
Raja <kraj9380286@gmail.com>
2025-06-24 16:52:04 +0530

    commited

--- pattern.c
+++ pattern.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
+//pass by reference
 int main(){
-    int a,b;
-    printf("Enter the nums:");
+    int temp,a,b;
+    printf("Enter the value :");
     scanf("%d",&a);
     scanf("%d",&b);
-    swap(a,b);
+    swap(&a,&b);
+    printf("swep num:%d%d",a,b);
 }
-void swap(int x,int y){
-    int temp;
-     temp=x;
-     x=y;
-     y=temp;
-     printf("Num is:%d %d",x,y);
-     
+void swap(int *x,int *y){
+   int temp;
+    temp=*x;
+    *x=*y;
+    *y=temp;
 }
