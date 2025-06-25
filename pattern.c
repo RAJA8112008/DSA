b18e65dbf6341e2c9a9481d29c7f7b63e349e573
Raja <kraj9380286@gmail.com>
2025-06-25 08:39:43 +0530

    commited

--- pattern.c
+++ pattern.c
@@ -5,16 +5,16 @@ void main(){
     for(i=0;i<n;i++){
         scanf("%d",&arr[i]);
     }
-    for(i=0;i<n-1;i++){
-        for(j=0;j<n-i-1;j++){
-            if(arr[j]>arr[j+1]){
-                temp=arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=temp;
-            }
+    for(i=1;i<n;i++){
+        temp=arr[i];
+        j=i-1;
+        while(j>=0 && arr[j]>temp){
+           arr[j+1]=arr[j];
+           j--;
         }
+        arr[j+1]=temp;
     }
-    for(i=0;i<n;i++){
+     for(i=0;i<n;i++){
         printf("%d ",arr[i]);
     }
 }
