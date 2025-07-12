0673eb02dfd70b67cd576d461bc1ce863f7a21be
Raja <kraj9380286@gmail.com>
2025-07-12 11:32:56 +0530

    commited

--- pattern.c
+++ pattern.c
@@ -1,27 +1,14 @@
 #include<stdio.h>
 void main(){
-    float a,b; 
-    char operator;
-    printf("Enter the operator:");
-    scanf(" %c",&operator);
-     printf("Enter the values:");
-     scanf("%f",&a);
-     scanf("%f",&b);
-     switch(operator){
-        case '+':
-        printf("result:%f",a+b);
-        break;
-        case '-':
-        printf("result:%f",a-b);
-        break;
-        case '*':
-        printf("result:%f",a*b);
-        break;
-        case '/':
-        if(b!=0)
-        printf("result:%.2f",a/b);
-        break;
-        default:
-        printf("invalid operator");
-     }
+    int st,end;
+    int sum=0;
+    printf("Enter starting value:");
+    scanf("%d",&st);
+     printf("Enter Ending value:");
+    scanf("%d",&end);
+
+    for(int st;st<=end;st++){
+        sum=sum+st;
+    }
+    printf("SUm is:%d",sum);
 }
