7f30cc5363fb70401b69e8c89a5c72f51e11ac96
Raja <kraj9380286@gmail.com>
2025-07-12 11:07:52 +0530

    commited

--- pattern.c
+++ pattern.c
@@ -1,16 +1,11 @@
 #include<stdio.h>
 void main(){
-    int a,b,c;
-    printf("Enter the num:");
-    scanf("%d",&a);
-    scanf("%d",&b);
-    scanf("%d",&c);
-    if(a>b && a>c){
-        printf("A is greater");
-    }else if(b>a && b>c){
-        printf("B is greater");
+    int num;
+    printf("Enter the num:\n");
+    scanf("%d",&num);
+    if(num%2==0){
+        printf("num is even");
     }else{
-        printf("C is greater");
+       printf("num is odd");
     }
-    
 }
