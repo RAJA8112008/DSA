2449fe111f59d52a8dd9035255ec60da6bcaa47a
Raja <kraj9380286@gmail.com>
2025-07-12 11:48:20 +0530

    commited

--- pattern.c
+++ pattern.c
@@ -1,11 +1,15 @@
 #include<stdio.h>
 void main(){
-    int n,fact=1;
-    printf("Enter the number");
-    scanf("%d",&n);
-    for(int i=1;i<=n;i++){
-        fact=fact*i;
+    int ed,evensum=0,oddsum=0;
+    printf("Enter the  ending value");
+    scanf("%d",&ed);
+    for(int i=1;i<=ed;i++){
+        if(i%2==0){
+            evensum=evensum+i;
+        }else{
+            oddsum=oddsum+i;
+        }
     }
-    printf("Factorial is:%d",fact);
-
+    printf("Sum of even num:%d\n",evensum);
+    printf("sum of odd num:%d",oddsum);
 }
