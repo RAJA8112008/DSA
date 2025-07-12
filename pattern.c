cc7014d2295607576d8fb9b08c52cb66001de840
Raja <kraj9380286@gmail.com>
2025-07-12 14:03:04 +0530

    commited

--- pattern.c
+++ pattern.c
@@ -1,26 +1,26 @@
 #include<stdio.h>
 void main(){
-    int arr[100],brr[100],n,result=0;
+    int arr[100],n;
     printf("Enter the size of Arrays:");
     scanf("%d",&n);
     printf("Enter the value of arr1:");
     for(int i=0;i<n;i++){
         scanf("%d",&arr[i]);
     }
-     printf("Enter the value of brr1:");
-    for(int i=0;i<n;i++){
-        scanf("%d",&brr[i]);
-    }
      printf("Array1 is:");
     for(int i=0;i<n;i++){
         printf("%d ",arr[i]);
     }
-     printf("\nArray2 is:");
-    for(int i=0;i<n;i++){
-        printf("%d ",brr[i]);
+   int min=arr[0];
+   int max=arr[0];
+   for(int i=0;i<n;i++){
+    if(min>arr[i]){
+        min=arr[i];
     }
-    for(int i=0;i<n;i++){
-        result +=arr[i]+brr[i];
+    if(max<arr[i]){
+        max=arr[i];
     }
-    printf("Sum:%d",result);
+   }
+   printf("min is:%d",min);
+    printf("max is:%d",max);
 }
