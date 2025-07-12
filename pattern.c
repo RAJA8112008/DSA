020dc4749fcce52d45577f5855a87530099ae6f4
Raja <kraj9380286@gmail.com>
2025-07-12 10:56:59 +0530

    commited

--- pattern.c
+++ pattern.c
@@ -1,16 +1,13 @@
 #include<stdio.h>
 void main(){
-    //swaping of two variables
-    int a,b,temp;
-    printf("Enter the number A:");
+    int a,b;
+    printf("Enter the numbers:");
     scanf("%d",&a);
-    printf("Enter the number B:");
-    scanf("%d",&b);
-    temp=a;
-    a=b;
-    b=temp;
-    printf("Swaped numbers are:\n");
-    printf(" a:%d",a);
-     printf("b:%d",b);
-    
+     scanf("%d",&b);
+     if(a==b){
+        printf("Equal:");
+     }else{
+        printf("not Equal:");
+     }
+
 }
