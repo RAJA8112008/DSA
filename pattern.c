6a99e755c6c1f3c286eef32f7bc4069920e31eba
Raja <kraj9380286@gmail.com>
2025-07-12 13:42:12 +0530

    commited

--- pattern.c
+++ pattern.c
@@ -1,19 +1,26 @@
 #include<stdio.h>
 void main(){
-    int arr[100],n,sum=0;
-    printf("Enter the size of array:");
+    int arr[100],brr[100],n,result=0;
+    printf("Enter the size of Arrays:");
     scanf("%d",&n);
-    printf("Enter the elements:");
+    printf("Enter the value of arr1:");
     for(int i=0;i<n;i++){
         scanf("%d",&arr[i]);
     }
-    printf("Array is:");
+     printf("Enter the value of brr1:");
+    for(int i=0;i<n;i++){
+        scanf("%d",&brr[i]);
+    }
+     printf("Array1 is:");
     for(int i=0;i<n;i++){
         printf("%d ",arr[i]);
     }
-     printf("Array Sum is:");
+     printf("\nArray2 is:");
     for(int i=0;i<n;i++){
-        sum=sum+arr[i];
+        printf("%d ",brr[i]);
     }
-      printf("%d",sum);
+    for(int i=0;i<n;i++){
+        result +=arr[i]+brr[i];
+    }
+    printf("Sum:%d",result);
 }
