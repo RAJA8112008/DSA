b299f0c34c38b62c43fd0be3640e9b5033f9e48a
Raja <kraj9380286@gmail.com>
2025-07-13 15:13:28 +0530

    commited

--- pattern.c
+++ pattern.c
@@ -1,29 +1,21 @@
 #include<stdio.h>
-void main(){
-    int arr[100][100],n,m;
-    printf("Enter the size of row:");
-     scanf("%d",&n);
-     printf("Enter the size of col:");
-     scanf("%d",&m);
-    printf("Enter the matrix values:");
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            scanf("%d",&arr[i][j]);
-        }
-    }
-    printf("Original matrix:\n");
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            printf("%d ",arr[i][j]);
-        }
-        printf("\n");
-    }
-printf("Transpose matrix:\n");
-for(int i=0;i<m;i++){
-    for(int j=0;j<n;j++){
-        printf("%d ",arr[j][i]);
-    }
-     printf("\n");
+int fibonaci(n){
+   if(n==0){
+    return 0;
+   }
+   if(n==1){
+    return 1;
+   }
+    return (n-1)+(n-2);
+
 }
 
+void main(){
+ int n;
+   printf("Enter the num:");
+   scanf("%d",&n);
+ for(int i=1;i<=n;i++){
+    int result=fibonaci(i);
+    printf("%d",result);
+ }
 }
