6cc479b9a73312d775e43db00e4ea4ba034bda26
Raja <kraj9380286@gmail.com>
2025-07-13 16:31:10 +0530

    commited

--- pattern.c
+++ pattern.c
@@ -1,18 +1,18 @@
 #include<stdio.h>
 void main(){
-    //pallindrome
-    int num,rem,sum=0,original;
-  printf("Enter the num:");
-  scanf("%d",&num);
+    int rem,num,sum=0,original;
+    printf("Enter the marks:");
+    scanf("%d",&num);
   original=num;
   while(num!=0){
     rem=num%10;
-    sum=sum*10+rem;
+    sum=sum+rem*rem*rem;
     num=num/10;
   }
-if(original==sum){
-    printf("Yes");
-}else{
-    printf("NO");
-}
+ if(sum==original){
+    printf("Pallindrome");
+ }else{
+    printf("Not Pallindrome");
+ }
+
 }
