39b48bf6b5b1246d9b545a6eed81d6d50b9bf9bf
Raja <kraj9380286@gmail.com>
2025-07-13 15:24:55 +0530

    commited

--- pattern.c
+++ pattern.c
@@ -1,21 +1,22 @@
 #include<stdio.h>
-int fibonaci(n){
-   if(n==0){
-    return 0;
-   }
-   if(n==1){
-    return 1;
-   }
-    return (n-1)+(n-2);
 
+int fibonaci(int n){
+    if(n==0){
+        return 0;
+    }
+    if(n==1){
+        return 1;
+    }
+    return fibonaci(n-1)+fibonaci(n-2);
 }
 
-void main(){
- int n;
-   printf("Enter the num:");
-   scanf("%d",&n);
- for(int i=1;i<=n;i++){
-    int result=fibonaci(i);
-    printf("%d",result);
- }
+
+int main(){
+    int n;
+    printf("Enter the value of n:");
+    scanf("%d",&n);
+    printf("Fibonaci seroies:");
+    for(int i=0;i<n;i++){
+        printf("%d ",fibonaci(i));
+    }
 }
