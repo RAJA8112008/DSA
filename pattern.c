908ddb58deee1777407aa587f52aa840fc8f4c08
Raja <kraj9380286@gmail.com>
2025-07-13 23:52:36 +0530

    commited

--- pattern.c
+++ pattern.c
@@ -1,48 +1,17 @@
 #include<stdio.h>
-void main(){
-    int arr[10][10],brr[10][10],crr[10][10],n,i,j,k;
-    printf("Enter the size of array:");
-    scanf("%d",&n);
-    for(i=0;i<n;i++){
-        for( j=0;j<n;j++){
-            scanf("%d",&arr[i][j]);
-        }
+int fact(int n){
+    if(n==0){
+        return 1;
     }
-    printf("First:");
-    for(i=0;i<n;i++){
-        for( j=0;j<n;j++){
-            printf("%d",arr[i][j]);
-        }
-         printf("\n");
+    if(n==1){
+        return 1;
     }
-    printf("second:");
-    for(i=0;i<n;i++){
-        for(j=0;j<n;j++){
-            scanf("%d",&brr[i][j]);
-        }
-        printf("\n");
-    }
-     printf("second:");
-    for(i=0;i<n;i++){
-        for( j=0;j<n;j++){
-            printf("%d",brr[i][j]);
-        }
-         printf("\n");
-    }
-    
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            crr[i][j]=0;
-            for(int k=0;k<n;k++){
-                 crr[i][j]+=arr[i][k]*brr[k][j];
-            }
-        }
-    }
-printf("MULTIPLY:");
-for(int i=0;i<n;i++){
-    for(int j=0;j<n;j++){
-        printf("%d ",crr[i][j]);
-    }
-    printf("\n");
+    return n*fact(n-1);
 }
+int main(){
+    int num;
+    printf("Enter the num:");
+    scanf("%d",&num);
+    printf("%d",fact(num));
+    
 }
