c106091a063793bcd2e9350406bda580d7b60c3c
Raja <kraj9380286@gmail.com>
2025-07-13 15:30:46 +0530

    commited

--- pattern.c
+++ pattern.c
@@ -1,22 +1,11 @@
 #include<stdio.h>
-
-int fibonaci(int n){
-    if(n==0){
-        return 0;
-    }
-    if(n==1){
-        return 1;
-    }
-    return fibonaci(n-1)+fibonaci(n-2);
-}
-
-
-int main(){
-    int n;
-    printf("Enter the value of n:");
-    scanf("%d",&n);
-    printf("Fibonaci seroies:");
-    for(int i=0;i<n;i++){
-        printf("%d ",fibonaci(i));
+void main(){
+    int year;
+    printf("Enter the value of year:");
+    scanf("%d",&year);
+    if((year%4==0&&year%100!=0)||year%400==0){
+        printf("Year is leap");
+    }else{
+        printf("Not leap");
     }
 }
