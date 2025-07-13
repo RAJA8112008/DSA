602ce50fa477a3389006fb933968c78f56c2388c
Raja <kraj9380286@gmail.com>
2025-07-13 20:20:42 +0530

    commited

--- pattern.c
+++ pattern.c
@@ -1,18 +1,17 @@
 #include<stdio.h>
- int fact(int n){
- if(n==0){
-    return 1;
- }
- if(n==1){
-    return 1;
- }
- return n*fact(n-1);
- }
-
-int main(){
-    int num,n;
-    printf("Enter the num:");
-    scanf("%d",&num);
-    int result=fact(num);
-    printf("%d",result);
+#include<stdlib.h>
+void main(){
+  int n;
+  printf("Enter the value of n: ");
+  scanf("%d",&n);
+  int *ptr=(int*)malloc(n*sizeof(int));
+  int *p=ptr;
+  for(int i=0;i<n;i++){
+    scanf("%d",&(*ptr));
+    ptr++;
+  }
+  for(int i=0;i<n;i++ ){
+    printf("%d ",*p);
+    p++;
+  }
 }
