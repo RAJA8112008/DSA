7422475d6292df800d44d5d18d03bb18e248ef30
Raja <kraj9380286@gmail.com>
2025-07-13 16:04:44 +0530

    commited

--- pattern.c
+++ pattern.c
@@ -1,22 +1,28 @@
 #include<stdio.h>
 void main(){
-    int marks;
-    printf("Enter the marks:");
-    scanf("%d",&marks);
-if(marks<100&&marks>=90){
-    printf("Grade A");
-}else if(marks<90&& marks>=80){
-    printf("Grade B");
-}
-else if(marks<80 && marks>=70){
-    printf("Grade C");
-}
-else if(marks<60 && marks>=50){
-    printf("Grade D");
-}
-else if(marks>=0 &&marks<49){
-    printf("Fail");
-}else{
-    printf("Invalid");
-}
+    int num1,num2;
+    char operator;
+    printf("Enter the num1:");
+    scanf("%d",&num1);
+     printf("Enter the num2:");
+    scanf("%d",&num2);
+    printf("Enterr the operator:");
+    scanf(" %c",&operator);
+   
+    switch(operator){
+        case '+':
+        printf("Sum:%d",num1+num2);
+        break;
+         case '-':
+        printf("Sub:%d",num1-num2);
+        break;
+         case '*':
+        printf("mul:%d",num1*num2);
+        break;
+         case '/':
+        printf("Sumb:%d",num1/num2);
+        break;
+        default:
+        printf("Invalid value");
+    }
 }
