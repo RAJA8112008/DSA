afe237267509b3595c0f817386a62afa311794f7
Raja <kraj9380286@gmail.com>
2025-07-13 15:40:39 +0530

    COMMITED

--- pattern.c
+++ pattern.c
@@ -1,11 +1,27 @@
 #include<stdio.h>
 void main(){
-    int year;
-    printf("Enter the value of year:");
-    scanf("%d",&year);
-    if((year%4==0&&year%100!=0)||year%400==0){
-        printf("Year is leap");
-    }else{
-        printf("Not leap");
+    int arr[100],target,n,i,count=0;
+    printf("Enter the size of array:");
+    scanf("%d",&n);
+    printf("Enter the Target:");
+    scanf("%d",&target);
+    printf("Enter the values of array:");
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
     }
+    printf("Array is:");
+    for(int i=0;i<n;i++){
+        printf("%d ",arr[i]);
+    }
+    for(int i=0;i<n;i++){
+       if(arr[i]==target){
+        count++;
+       }
+    }
+    if(count==1){
+        printf("YES");
+    }else{
+        printf("NO");
+    }
+   
 }
