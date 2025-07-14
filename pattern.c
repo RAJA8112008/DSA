79c1a25105beac6d4beb3071e4acdf158fbf1243
Raja <kraj9380286@gmail.com>
2025-07-14 07:20:35 +0530

    commited

--- pattern.c
+++ pattern.c
@@ -1,33 +1,17 @@
 #include<stdio.h>
-// void swap(int *a,int *b){
-//  int temp;
-//  if(*a>*b){
-//     temp=*a;
-//     *a=*b;
-//     *b=temp;
-//  }
-// }
-int main(){
- int arr[10],n,temp;
- printf("Enter the size:");
- scanf("%d",&n);
-  for(int i=0;i<n;i++){
-    scanf("%d",&arr[i]);
-  }
-  for(int i=0;i<n;i++){
-    printf("%d ",arr[i]);
-  }
-  //bubble sort
- for(int i=0;i<n;i++){
-    for(int j=0;j<n-i-1;j++){
-        if(arr[j]>arr[j+1]){
-        temp=arr[j];
-        arr[j]=arr[j+1];
-        arr[j+1]=temp;
+
+void main(){
+    int num,count=0;
+    printf("Enter the num:");
+    scanf("%d",&num);
+    for(int i=1;i<=num;i++){
+        if(num%i==0){
+            count++;
         }
     }
- }
- for(int i=0;i<n;i++){
-    printf("%d",arr[i]);
- }
+    if(count==2){
+        printf("YES");
+    }else{
+        printf("NO");
+    }
 }
